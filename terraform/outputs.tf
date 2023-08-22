3e444aaba8590fa2510805d18d9758b4082dac95
Jeremy Cook <jeremy.cook@cloudacademy.com>
2023-08-22 13:49:54 +1200

    updated outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,7 +2,12 @@ output "echo-frontend-url-command" {
   value = "echo http://$(kubectl get ing -n cloudacademy frontend -o jsonpath='{.spec.rules[0].host}')"
 }
 
-output "web_app_wait_command" {
-  value       = "until curl -Is --max-time 5 http://$(kubectl get ing -n cloudacademy frontend -o jsonpath='{.spec.rules[0].host}')/api/stocks/csv | grep 'HTTP/1.1 200'; do echo preparing...; sleep 5; done; echo; echo -e 'Ready...'"
+output "api_test_wait_command" {
+  value       = "until curl -Is --max-time 5 http://$(kubectl get ing -n cloudacademy api -o jsonpath='{.spec.rules[0].host}')/api/stocks/csv | grep 'HTTP/1.1 200'; do echo preparing...; sleep 5; done; echo; echo -e 'Ready...'"
+  description = "Test command - tests readiness of the web app"
+}
+
+output "app_test_wait_command" {
+  value       = "until curl -Is --max-time 5 http://$(kubectl get ing -n cloudacademy frontend -o jsonpath='{.spec.rules[0].host}')/ | grep 'HTTP/1.1 200'; do echo preparing...; sleep 5; done; echo; echo -e 'Ready...'"
   description = "Test command - tests readiness of the web app"
 }

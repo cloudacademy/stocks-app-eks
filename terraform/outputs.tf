49cfeca624cb20bfeb8ee86098b6c90632da5952
Jeremy Cook <jeremy.cook@cloudacademy.com>
2023-09-20 21:56:55 +1200

    minor updates

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,7 +2,6 @@ output "echo-frontend-url-command" {
   value = "echo http://$(kubectl get ing -n cloudacademy public -o jsonpath='{.spec.rules[0].host}')"
 }
 
-
 output "app_test_wait_command" {
   value       = "until curl -Is --max-time 5 http://$(kubectl get ing -n cloudacademy public -o jsonpath='{.spec.rules[0].host}')/ | grep 'HTTP/1.1 200'; do echo preparing...; sleep 5; done; echo; echo -e 'Ready...'"
   description = "Test command - tests readiness of the web app"
